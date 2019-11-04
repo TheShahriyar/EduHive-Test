715397cc1e6e147aa01af2f75a30e6a5196de70e
Shahriyar Ahmed <shahriyar.31dec@gmail.com>
2019-11-04 11:04:49 +0600

    Animated Counter Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
+(function($) {
+  "use strict";
 
+  //when dom is ready
+  $(document).ready(function() {
+    // Start Animated Number
+    $(".counter-right").appear(function() {
+      $(".animated-number").countTo({
+        speed: 4000,
+        refreshInterval: 60,
+        formatter: function(value, options) {
+          return value.toFixed(options.decimals);
+        }
+      });
+    });
+  });
+  //dom ready end
+})(jQuery);
